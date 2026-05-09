65ee5a9fdaf63924183ac41c3c77fea88d28cc6e
rahul p <rahulyts5@gmail.com>
2026-05-09 22:51:00 +0530

    fix api url

--- src/config.js
+++ src/config.js
@@ -1,1 +1,1 @@
-export const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://vastrika-nx-backend.test/api'
+export const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://vastrika-nx-backend.onrender.com/api'
